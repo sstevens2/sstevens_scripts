ff319039bba72611e02b8c845d503bafcd6bfe06
Sarah Stevens <sstevens2@wisc.edu>
2015-02-10 15:53:40 -0600

    Added docstring to begining of combine_phylosift.py

--- jgi_scripts_removeasneeded/combine_phylosiftout.py
+++ jgi_scripts_removeasneeded/combine_phylosiftout.py
@@ -2,6 +2,9 @@
 
 import sys, os
 
+"""combine_phylosift.py: takes a directory with many files from parse_phylosift_sts.py and 
+	makes a tab-delimited file with the taxonomy of each listed"""
+
 def usage():
 	print "Usage: combine_phylosiftout.py  directoryofparsedSTSs"
 
