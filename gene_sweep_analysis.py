22323bdeff5956218448a541b99a9f8f007e3264
Sarah Stevens <sstevens2@wisc.edu>
2015-03-03 08:37:22 -0600

    Started gene_sweep_analysis.py and added program header info

---
+++ gene_sweep_analysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys, os
+
+"""gene_sweep_analysis.py: looking for regions where SNPs sweep"""
+
+__author__ = "Sarah Stevens"
+__email__ = "sstevens2@wisc.edu"
+
+def usage():
+	print "Usage: gene_sweep_analysis.py <file_TFfixed.tsv>"
+
+if len(sys.argv) != 2:
+	usage()
+	sys.exit(2)
