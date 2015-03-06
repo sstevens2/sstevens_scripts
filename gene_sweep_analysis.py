91e3e343ca50326734d6c5e57b0d343e94e2cbeb
Sarah Stevens <sstevens2@wisc.edu>
2015-03-06 10:31:30 -0600

    some rearranging in gene_sweep_analysis, also added make_indexlist function to find all the indices which are in a sweeping region(in any year)

--- gene_sweep_analysis.py
+++ gene_sweep_analysis.py
@@ -21,7 +21,7 @@ input = pd.read_table(sys.argv[1],sep='\t')
 reg_value = int(sys.argv[2]) # sets size of region to check for sweep, ex. 3 SNPs in a row
 ##print input['Y2005']
 
-def check_sweep(input_list, reg):
+def check_sweep(input_list, reg): #checks each sliding window to see if there are the right number of trues in a row
 	outlist=[]
 	for i, row in enumerate(input_list):
 		if i < (len(input_list)-(reg-1)):
@@ -32,6 +32,17 @@ def check_sweep(input_list, reg):
 			else:
 				outlist.append(i)
 	return outlist
+
+def make_indexlist(sweepfilt_dict, reg): #makes list of all the rows which are actually in the gene region that sweeps
+	all_sweep=list() #all the lines which sweep
+	for item in sweepfilt_dict:
+##		print item, sweepfilt_dict[item]
+		for i in sweepfilt_dict[item]:
+			for j in range(0,reg):
+				index=j+i
+				if index not in all_sweep:
+					all_sweep.append(index)
+	return sorted(all_sweep)
 
 ##check_sweep(input['Y2005'])
 years = ['Y2005', 'Y2007', 'Y2008', 'Y2009', 'Y2012', 'Y2013']
@@ -48,13 +59,9 @@ for name in years[1:]:
 	sweeps_set=list(set(sweep_dict[name]) & (set(sweep_dict[name]) ^ set(sweep_dict[years[0]])))
 	if sweeps_set:
 		sweep_filt[name]=sweeps_set
-		for item in sweeps_set:
-			if item not in all_sweep:
-				all_sweep.append(item)
-			if (item+1) not in all_sweep:
-				all_sweep.append(item)
-			if (item+2) not in all_sweep:
-				all_sweep.append(item)
+
+swept_regions=make_indexlist(sweep_filt, reg_value)
+print swept_regions
 
 ##print list(set(all_sweep))
 ##print sweep_filt
