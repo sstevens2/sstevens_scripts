1b3ab72cde38842da10fd2d3c617adb91abc8856
Sarah Stevens <sstevens2@wisc.edu>
2015-03-09 14:12:45 -0500

    gene_sweep_analaysis will now write 'low_cov' whenever the coverage for that column is below 10

--- gene_sweep_analysis.py
+++ gene_sweep_analysis.py
@@ -9,10 +9,10 @@ __author__ = "Sarah Stevens"
 __email__ = "sstevens2@wisc.edu"
 
 def usage():
-	print "Usage: gene_sweep_analysis.py file_TFfixed.tsv windowsize"
+	print "Usage: gene_sweep_analysis.py file_TFfixed.tsv windowsize covfile"
 	print "windowsize specifies how many SNPs in a row must sweep in a region, must be an integer and > 1"
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
 	usage()
 	sys.exit(2)
 
@@ -20,6 +20,7 @@ if len(sys.argv) != 3:
 input = pd.read_table(sys.argv[1],sep='\t')
 reg_value = int(sys.argv[2]) # sets size of region to check for sweep, ex. 3 SNPs in a row
 years = ['Y2005', 'Y2007', 'Y2008', 'Y2009', 'Y2012', 'Y2013']
+covfile= pd.read_table(sys.argv[3],sep='\t')
 
 #functions
 def check_sweep(input_list, chrom_list, reg): #checks each sliding window to see if there are the right number of trues in a row, provided they are on the same contig
@@ -71,16 +72,24 @@ for item in sweep_filt:
 	counts[item] = len(sweep_filt[item])
 #print counts
 
-#writing counts to output
+#getting the coverage for that year
 filename=sys.argv[1].split('_')[0]
+covs = covfile[covfile['id'] == filename]
+covs = covs > 10
+
+#writing counts to output those which pass the coverage requirement above
 header='window_size\t'
 outline=filename+'_'+str(reg_value)+'\t'
 for name in years: # loop to put together header and output line of counts
 	header=header+name+'\t'
-	if name in counts: #checks if year has any count
+	cyear = 'X'+name[1:] #year for coverage
+	cov = covs[cyear]
+	if (name in counts) and (cov.iloc[0] == True): #checks if year has any count
 		outline=outline+str(counts[name])+'\t'
 	elif name == years[0]: # writes na if this is the first year
 		outline=outline+'na'+'\t'
+	elif (cov.iloc[0] == False) and (name != years[0]):
+		outline=outline+'low_cov'+'\t'
 	else: #writes 0 if there is were no windows found
 		outline=outline+'0'+'\t'
 
