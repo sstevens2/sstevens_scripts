f229ec80dad499026e6365ff612db3917c202bd5
Sarah Stevens <sstevens2@wisc.edu>
2015-10-06 17:10:28 -0500

    Updated some string usage

--- AAI_BLASTnPARSE.py
+++ AAI_BLASTnPARSE.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
 
-import sys, os
+import sys, os, glob
 
 def usage():
-	print "Usage: ANI_BLASTnPARSE_ANI.py folder2faa"
+	print "Usage: ANI_BLASTnPARSE_ANI.py 'glob4files"
 	print "Runs for protein seqs"
 
 if len(sys.argv) != 2:
 	usage()
 	exit()
 
-path2faa= sys.argv[1]
+path2faa= os.path.split(sys.argv[1])[0]
+filelist=glob.glob(path2faa)
 
 #all v all blast
 for file in filelist:
 	for file2 in filelist:
 		file1n=path2faa+file
 		file2n=path2faa+".db"
-		outname=file1n.split(".faa")[0]+"v"+file2.split(".faa")[0]+".blast"
-		print "BLASTING "+file+" v "+file2
-		os.system("blastp -task blastp -query " + file1n+" -db "+file2n+" -out "+outname+" -evalue 0.001")
-		os.system("filtersearchio5 -tophsp -qcoverage 50 -format 8 < " +outname+" > "+ outname+".tophsp.percqcov50")
+		outname=os.path.splitext(file1n)[0]+"v"+os.path.splitext(file2.)[0]+".blast"
+		print("BLASTING {} v {}".format(file1,file2))
+		os.system("blastp -task blastp -query {} -db {} -out {} -evalue 0.001".format(file1n,file2n,outname))
+		os.system("filtersearchio5 -tophsp -qcoverage 50 -format 8 < {} > {}.tophsp.percqcov50".format(outname))
 
 for file in filelist:
 	for file2 in filelist:
