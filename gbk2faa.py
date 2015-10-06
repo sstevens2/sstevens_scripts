d0d2a57cd9b1414a5a397d78d0acd2993ded1584
Sarah Stevens <sstevens2@wisc.edu>
2015-10-06 14:03:14 -0500

    Updated from online suggestion and added format for strs

--- gbk2faa.py
+++ gbk2faa.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os
+from Bio import SeqIO
 
 def usage():
 	print "Usage: gbk2faa.py gbkfile"
@@ -13,4 +14,19 @@ if len(sys.argv) != 3:
 gbk = sys.argv[1]
 
 outname=file.split(".gb")[0]
-os.system("cat "+path2gbk+file+"| gex2.pl cds > "+path2fna+outname+".faa")
+
+gbk_filename = sys.argv[1]
+faa_filename = os.path.splitext(sys.argv[1])+'.faa'
+input  = open(gbk_filename, "r")
+output = open(faa_filename, "w")
+
+for seq_record in SeqIO.parse(input, "genbank") :
+    print "Dealing with GenBank record {0}".format(seq_record.id)
+    for seq_feature in seq_record.features :
+        if seq_feature.type=="CDS" :
+            assert len(seq_feature.qualifiers['translation'])==1
+            output.write(">{loctag}\n{seq}\n".format(
+                   loctag=seq_feature.qualifiers['locus_tag'][0],seq=
+                   seq_feature.qualifiers['translation'][0]))
+output.close()
+input.close()
