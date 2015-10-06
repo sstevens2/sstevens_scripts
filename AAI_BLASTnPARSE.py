d79cf782949868f35c0359b5d655f02ab2693f0b
Sarah Stevens <sstevens2@wisc.edu>
2015-10-06 17:36:46 -0500

    Updated more string parsing and now works

--- AAI_BLASTnPARSE.py
+++ AAI_BLASTnPARSE.py
@@ -3,7 +3,7 @@
 import sys, os, glob
 
 def usage():
-	print "Usage: ANI_BLASTnPARSE_ANI.py 'glob4files"
+	print "Usage: ANI_BLASTnPARSE_ANI.py 'glob4files'"
 	print "Runs for protein seqs"
 
 if len(sys.argv) != 2:
@@ -11,16 +11,16 @@ if len(sys.argv) != 2:
 	exit()
 
 path2faa= os.path.split(sys.argv[1])[0]
-filelist=glob.glob(path2faa)
+filelist=glob.glob(sys.argv[1])
 
 #all v all blast
 for file in filelist:
 	for file2 in filelist:
-		file1n=path2faa+file
-		file2n=path2faa+".db"
-		outname=os.path.splitext(file1n)[0]+"v"+os.path.splitext(file2.)[0]+".blast"
-		print("BLASTING {} v {}".format(file1,file2))
-		os.system("blastp -task blastp -query {} -db {} -out {} -evalue 0.001".format(file1n,file2n,outname))
+		file1n=os.path.split(file)[1]
+		file2n=os.path.split(file2)[1]
+		outname="{}v{}.blast".format(os.path.splitext(file1n)[0],os.path.splitext(file2n)[0])
+		print("BLASTING {} v {}".format(file1n,file2n))
+		os.system("blastp -task blastp -query {} -db {} -out {} -evalue 0.001".format(file,file2n+'.db',outname))
 		os.system("filtersearchio5 -tophsp -qcoverage 50 -format 8 < {} > {}.tophsp.percqcov50".format(outname))
 
 for file in filelist:
