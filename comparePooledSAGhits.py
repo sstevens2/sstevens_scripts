96a6e8f93e6eba6a097f80afdc9270aac04fd732
Sarah Stevens <sstevens2@wisc.edu>
2016-02-04 16:25:58 -0600

    removed duplicate line

--- comparePooledSAGhits.py
+++ comparePooledSAGhits.py
@@ -42,7 +42,6 @@ for ref in refs:
         ref2_df=input[input['SAG'] == ref2]
         if ref == ref2: # Sanity check.. the same SAG vs itself should return the number of unique reads for that SAG
             assert len(intersectHits(ref_df['READ'], ref2_df['READ'])) == len(set(ref_df['READ']))
-        ref2_df=input[input['SAG'] == ref2]
         hitsout_df.set_value(ref,ref2,len(intersectHits(ref_df['READ'], ref2_df['READ'])))
         #Add percentage version...
         percout_df.set_value(ref,ref2,(len(intersectHits(ref_df['READ'], ref2_df['READ']))/float(len(set(ref_df['READ'])))))
