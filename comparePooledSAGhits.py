d2dd515cecc531c9a423725f89dee2d1d6cac016
Sarah Stevens <sstevens2@wisc.edu>
2016-02-12 20:35:52 -0600

    Many changes
    Now can take both pooled an unpooled (will change anme to reflect soon
    Also uses arg parse
    Also uses python 2.7 in order to use map(can't get the map part to work with python3)
    Almost ready to use multiprocessing (has arg but not acutally processing)

--- comparePooledSAGhits.py
+++ comparePooledSAGhits.py
@@ -1,25 +1,21 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python
 
-import sys, os
-import pandas as pd
+import sys, os, pandas as pd, argparse
 
 """compareSAGhits.py: finding out how many reads hit to different SAGs, from blast results"""
 
 __author__ = "Sarah Stevens"
 __email__ = "sstevens2@wisc.edu"
 
-def usage():
-	print("Usage: comparePooledSAGhits.py catblastfile")
-	print("finds out how many reads both of a pair SAGs, from blast results")
-	print("catblastfile: file with all the blast results from the different genomes to compare")
-	print("If you want to see detailed info (which SAG hit it better, difference, etc.), must input best blast hit filter results (bbh)")
-	print("You can use blast_besthit(pooled!).py to get this new file")
-
-if len(sys.argv) != 2:
-	usage()
-	sys.exit(2)
-
 # Functions:
+
+def parseArgs():
+	parser = argparse.ArgumentParser(description='compareSAGhits.py: finding out how many reads hit to different SAGs, from blast results, has extended output for bbh files')
+	parser.add_argument('--blast_in','-bin' , action="store", dest='blast_in', type=str, required=True, metavar='catblastoutfm6', help="This should be the concatenated blast file of the different SAGs. Extended functionality if the file is bbh's for each SAG.")
+	parser.add_argument('--pooled','-p', action="store_true", dest='pool', default=False, help='If you are using a pooled file, add this flag')
+	parser.add_argument('--num_threads', action="store", dest='num_threads',type=int, default=1, metavar='num', help='Number of threads to use if multiprocessing')
+	args=parser.parse_args()
+	return args.blast_in , args.pool, args.num_threads
 
 # Function to intersect two Series of Readnames and return shared set
 def intersectHits(s1, s2):
@@ -35,7 +31,7 @@ def parseInput(comb_df,parseCol):
 	for ref in refs:
 		ref_df=comb_df[comb_df[parseCol] == ref]
 		out_dict[ref]=ref_df
-		if len(ref_df['READ'].unique()) != len(ref_df['READ']):
+		if len(ref_df['read'].unique()) != len(ref_df['read']):
 			bbhbool = False
 	return out_dict, bbhbool
 
@@ -44,22 +40,22 @@ def compareHits(refs):
 	ref, ref2 = refs
 	ref_df = infile_parsed[ref] 
 	ref2_df= infile_parsed[ref2]
-	matchingReads = intersectHits(ref_df['READ'], ref2_df['READ'])
+	matchingReads = intersectHits(ref_df['read'], ref2_df['read'])
 	if ref == ref2: # Sanity check.. the same SAG vs itself should return the number of unique reads for that SAG
-		assert len(matchingReads) == len(set(ref_df['READ']))
+		assert len(matchingReads) == len(set(ref_df['read']))
 	# Adding number of hits to output df
 	hitsout_df.set_value(ref,ref2,len(matchingReads))
 	#Add percentage version...
-	perc = (len(matchingReads)/float(len(set(ref_df['READ']))))
+	perc = (len(matchingReads)/float(len(set(ref_df['read']))))
 	percout_df.set_value(ref,ref2,perc)
 	# Who hit it best analysis
 	if isBBH:
 		## subset the two refs down to only the shared hits
-		ref1match = ref_df[ref_df['READ'].isin(matchingReads)]#.sort_values('READ')
-		ref2match = ref2_df[ref2_df['READ'].isin(matchingReads)]#.sort_values('READ')
+		ref1match = ref_df[ref_df['read'].isin(matchingReads)]#.sort_values('read')
+		ref2match = ref2_df[ref2_df['read'].isin(matchingReads)]#.sort_values('read')
 		assert len(ref1match) == len(ref2match) #just a sanity check
-		#assert (ref1match['READ'] == ref2match['READ']).all() # are the reads the same and in the same order, not sure this matters with join...
-		merged_df = pd.merge(ref1match, ref2match, on='READ', how='inner')
+		#assert (ref1match['read'] == ref2match['read']).all() # are the reads the same and in the same order, not sure this matters with join...
+		merged_df = pd.merge(ref1match, ref2match, on='read', how='inner')
 		## Count the hits where ID is equal
 		equal = (merged_df['bit_score_x'] - merged_df['bit_score_y'] == 0).sum() # number of hits which have equal bit score
 		## Count the hits where ID ref1>ref2
@@ -81,43 +77,38 @@ def compareHits(refs):
 		bbh_out.write(sep.join((ref, ref2, str(len(matchingReads)), str(perc), str(equal), str(ref1greater.sum()), str(ref1less.sum()), str(ref1greaterDiffPID.max()), str(ref1greaterDiffPID.mean()), str(ref1greaterDiffPID.min()), str(ref1greaterDiffBS.max()), str(ref1greaterDiffBS.mean()), str(ref1greaterDiffBS.min()), str(ref1lessDiffPID.max()), str(ref1lessDiffPID.mean()), str(ref1lessDiffPID.min()), str(ref1lessDiffBS.max()), str(ref1greaterDiffBS.mean()), str(ref1lessDiffBS.min()),'\n')))
 
 # Main:
-
-# Read in file as DataFrame
-infile = pd.read_table(sys.argv[1],delim_whitespace=True, header=None, index_col=False, names=['pool','meta_info','ref_info','PID','align_len','mismatches','gaps','q_start','q_end','s_start','s_end','evalue','bit_score'])
-
-# Add a new column that is just the Readname split from the 1st[1] column
-infile['READ']=infile['meta_info'].str.split('.blast:').str.get(1)
-# Add a new column that is just the SAG name from the 2nd[2] column
-infile['SAG']=infile['ref_info'].str.split('_').str.get(0) # need if using value counts later
-
-# dictionary to store all of the SAGs and their df (so we aren't subsetting many times)
-infile_parsed, isBBH = parseInput(comb_df=infile, parseCol='SAG')
-
-# Make output DataFrames
-names=list(infile_parsed.keys())
-hitsout_df=pd.DataFrame(index=names, columns=names) # Raw number of hits
-percout_df=pd.DataFrame(index=names, columns=names) # Percentage of ref's hits
-
-
-if isBBH:
-	print('Your file is a bbh, running extra analysis')
-	bbh_out = open(sys.argv[1]+'.compALL', 'w')
-	bbh_out.write('ref1\tref2\tnum_hits\tperc_hits\tsame\tref1>2\tref1<2\tref1>2_PID_max\tref1>2_PID_mean\tref1>2_PID_min\tref1>2_BS_max\tref1>2_BS_mean\tref1>2_BS_min\tref1<2_PID_max\tref1<2_PID_mean\tref1<2_PID_min\tref1<2_BS_max\tref1<2_BS_mean\tref1<2_BS_min\n')
-else:
-	print('Your file is not a bbh, you will only get the .compHIT and .compPERC files')
-
-allCombos = list() # list that holds all possible combinations of sag v. sags
-# For each SAG
-for ref in names:
-	# For each other SAG
-	for ref2 in names:
-		allCombos.append([ref,ref2])
-
-map(compareHits, allCombos)
-		
-
-# Write *out_df's to files
-hitsout_df.to_csv(sys.argv[1]+'.compHIT', sep='\t')
-percout_df.to_csv(sys.argv[1]+'.compPERC', sep='\t')
-if isBBH:
-	bbh_out.close()
+if __name__ == '__main__':
+	filename, pooled, num_thr = parseArgs()
+	# Read in file as DataFrame
+	if pooled:
+		infile = pd.read_table(filename,delim_whitespace=True, header=None, index_col=False, names=['pool','meta_info','ref_info','PID','align_len','mismatches','gaps','q_start','q_end','s_start','s_end','evalue','bit_score'])
+		# Add a new column that is just the Readname split from the 1st[1] column
+		infile['read']=infile['meta_info'].str.split('.blast:').str.get(1)
+	else:
+		input = pd.read_table(filename,delim_whitespace=True, header=None, index_col=False, names=['read','ref_info','PID','align_len','mismatches','gaps','q_start','q_end','s_start','s_end','evalue','bit_score'])
+	# Add a new column that is just the SAG name from the 2nd[2] column
+	infile['SAG']=infile['ref_info'].str.split('_').str.get(0) # need if using value counts later
+	# dictionary to store all of the SAGs and their df (so we aren't subsetting many times)
+	infile_parsed, isBBH = parseInput(comb_df=infile, parseCol='SAG')
+	# Make output DataFrames
+	names=list(infile_parsed.keys())
+	hitsout_df=pd.DataFrame(index=names, columns=names) # Raw number of hits
+	percout_df=pd.DataFrame(index=names, columns=names) # Percentage of ref's hits
+	if isBBH:
+		print('Your file is a bbh, running extra analysis')
+		bbh_out = open(filename+'.compALL', 'w')
+		bbh_out.write('ref1\tref2\tnum_hits\tperc_hits\tsame\tref1>2\tref1<2\tref1>2_PID_max\tref1>2_PID_mean\tref1>2_PID_min\tref1>2_BS_max\tref1>2_BS_mean\tref1>2_BS_min\tref1<2_PID_max\tref1<2_PID_mean\tref1<2_PID_min\tref1<2_BS_max\tref1<2_BS_mean\tref1<2_BS_min\n')
+	else:
+		print('Your file is not a bbh, you will only get the .compHIT and .compPERC files')
+	allCombos = list() # list that holds all possible combinations of sag v. sags
+	# For each SAG
+	for ref in names:
+		# For each other SAG
+		for ref2 in names:
+			allCombos.append([ref,ref2])
+	map(compareHits, allCombos)
+	# Write *out_df's to files
+	hitsout_df.to_csv(filename+'.compHIT', sep='\t')
+	percout_df.to_csv(filename+'.compPERC', sep='\t')
+	if isBBH:
+		bbh_out.close()
