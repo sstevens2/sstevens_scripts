c7a3fdf7dbc2070d2c5df17e8c392423c43d1f21
Sarah Stevens <sstevens2@wisc.edu>
2016-02-15 11:13:38 -0600

    Updated to use pool with multithreading

--- blastSAGVMetas.py
+++ blastSAGVMetas.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python
 
-import sys, os
+import sys, os, glob, subprocess
+from multiprocessing import Pool
 
 def usage():
-	print "Usage: blastSAGVMetas.py SAGfasta pathtoMetas"
+	print "Usage: blastSAGVMetas.py 'path2SAGsfasta(glob)' 'metagenomes(glob)' threads(int)"
 	print "Makes db and runs blastn with the following settings"
-	print "-evalue 0.001 -perc_identity 95 -outfmt 6"
-	
+	print "-evalue 0.001 -outfmt 6 -perc_identity 95"
 
-
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
 	usage()
 	exit()
 	
-SAGname=sys.argv[1]
-path2metas=sys.argv[2]
+path2SAGs=sys.argv[1]
+path2Metas=sys.argv[2]
+threads=int(sys.argv[3])
 
+def call_blast(params):
+	SAGname, metaname = params
+	SAGdb=SAGname+".db"
+	outSAG=os.path.basename(os.path.splitext(SAGname)[0])
+	outmeta=os.path.basename(os.path.splitext(metaname)[0])
+	outname=outmeta+'-vs-'+outSAG+'.blast'
+	cmd=['blastn', '-task blastn', '-db '+SAGdb, '-query '+metaname,'-out '+outname, '-evalue 0.001', '-outfmt 6','-perc_identity 95']
+	print ' '.join(cmd), os.getpid() #testing line, left in for log file
+	os.system(' '.join(cmd)) #replace with subprocess when you figure it out
+	#subprocess.call(cmd)
 
-os.system("makeblastdb -in "+SAGname+" -out "+SAGname+".db -dbtype nucl")
-#print("makeblastdb -in "+SAGname+" -out "+SAGname+".db -dbtype nucl")
-SAGdb=SAGname+".db"
-for metaname in os.listdir(path2metas):
-	if metaname.endswith(".fasta"):
-		outmeta=metaname.split(".fasta")[0]
-		outSAG=SAGname.split(".fasta")[0]
-#		print("blastn -task blastn -db "+SAGdb+" -query "+path2metas+metaname+" -out "+outmeta+"-vs-"+outSAG+ ".blast -evalue 0.001 -perc_identity 95 -outfmt 6")
-		os.system("blastn -task blastn -db "+SAGdb+" -query "+path2metas+metaname+" -out "+outmeta+"-vs-"+outSAG+ ".blast -evalue 0.001 -perc_identity 95 -outfmt 6")
+if __name__ == '__main__':
+	p=Pool(threads)
+	#print path2SAGs
+	SAGfiles=glob.glob(path2SAGs)
+	metafiles=glob.glob(path2Metas)
+	#print SAGfiles
+	#all possible combos of SAGs vs Metagenomes
+	inlist=[]
+	for SAG in SAGfiles:
+		os.system('makeblastdb -in '+SAG+' -out '+SAG+'.db'+' -dbtype nucl')
+		for meta in metafiles:
+			inlist.append([SAG, meta])
+	#print inlist
+	p.map(call_blast,inlist)
+	p.close()
+
